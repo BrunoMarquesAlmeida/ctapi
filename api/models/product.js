974ae184e633d1aec226612803cd2e7e82a566fd
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-03 18:32:42 +0000

    modified product schema

--- api/models/product.js
+++ api/models/product.js
@@ -4,6 +4,12 @@ const productSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   title: { type: String, required: true },
   precos: { type: Object, required: true },
+  description: Array,
+  categorias: { type: Object, required: true },
+  specs: Array,
+  img: Array,
+  ribbons: Object,
+  vendas: Number,
 });
 
 module.exports = mongoose.model("Product", productSchema);
