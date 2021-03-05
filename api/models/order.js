093341343672344d148d42f80ec3925ec9a2cc04
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-05 10:39:05 +0000

    Updated orderChema

--- api/models/order.js
+++ api/models/order.js
@@ -4,6 +4,10 @@ const orderSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   address: { type: Object, required: true },
   items: { type: Object, required: true },
+  delivery: { type: Object, required: true },
+  payment: { type: Object, required: true },
+  totals: { type: Object, required: true },
+  status: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Order", orderSchema);
