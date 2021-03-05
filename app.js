c0fe5cef2faff2702a29850a1c40b27feb022ebb
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-05 10:38:50 +0000

    Updated comments; Corrected typo in Access-Control-Allow-Headers

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const express = require("express");
 const app = express();
 
-// live updating for development
+// live logging
 const morgan = require("morgan");
 
 // process request body
 const bodyParser = require("body-parser");
 
-//mongo DB object modeling
+//mongo DB connecting and object modeling
 const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
@@ -27,12 +27,13 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Acess-Control-Allow-Headers",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
 
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+
     return res.status(200).json({});
   }
 
