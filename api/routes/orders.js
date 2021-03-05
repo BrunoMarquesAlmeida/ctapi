e3eab44395cafad7ace70a7a6b761110eefa1479
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-05 10:40:30 +0000

    Updated orders GET, GET by id, POST; Added a Delete everything route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const Order = require("../models/order");
+const Product = require("../models/product");
 
-router.get("/", (req, res, next) => {
+router.get("/", (req, res) => {
   Order.find()
-    .select("address items _id")
+    .select("address items _id payment delivery totals status")
     .exec()
     .then((orders) => {
       res.status(200).json(orders);
@@ -18,13 +19,13 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", (req, res) => {
   const { id } = req.params;
 
   Order.findById(id)
     .exec()
-    .then(({ _id, address }) => {
-      res.status(200).json({ _id, address });
+    .then(({ _id, address, items, delivery, payment, totals }) => {
+      res.status(200).json({ _id, address, items, delivery, payment, totals });
     })
     .catch((err) => {
       res.status(500).json({ err: err });
@@ -36,22 +37,45 @@ router.post("/", (req, res, next) => {
     _id: mongoose.Types.ObjectId(),
     address: req.body.address,
     items: req.body.items,
+    payment: req.body.payment,
+    delivery: req.body.delivery,
+    totals: req.body.totals,
+    status: "Em preparação",
+  });
+
+  Object.values(req.body.items).map(({ _id }) => {
+    Product.updateOne({ _id }, { $inc: { vendas: 1 } })
+      .exec()
+      .then()
+      .catch((err) => res.status(500).json({ err }));
   });
 
   order
     .save()
-    .then(({ id, address }) => {
+    .then((result) => {
       res.status(201).json({
         message: "Order stored",
-        result: { id, address },
+        result,
       });
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json({
-        err: err,
+        err,
       });
     });
 });
+
+// DELETE ALL ORDERS
+// router.delete("/", (req, res) => {
+//   Order.remove({})
+//     .exec()
+//     .then(() => {
+//       res.status(200).json({
+//         message: "All orders were deleted",
+//       });
+//     })
+//     .catch((err) => res.status(500).json(err));
+// });
 
 module.exports = router;
