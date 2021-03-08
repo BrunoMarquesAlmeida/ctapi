911b2ff15202c626530da40d052301359ea97774
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-08 19:24:40 +0000

    added .get by userId

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -34,6 +34,20 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+
+  Order.find({ userId: id })
+    .select("address items _id payment delivery totals status userId")
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: err });
+    });
+});
+
 router.post("/", (req, res, next) => {
   const order = new Order({
     _id: mongoose.Types.ObjectId(),
